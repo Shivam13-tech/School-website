c2feb15fb6d5889fbf31a21d6f00b4a93bab5e46
Shivam <shivampandey88@hotmail.com>
2022-09-10 22:26:47 +0530

    Completed contact page

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,63 @@
+
+function Contact(){
+    return (
+        <div>
+            <div className="Location">
+                <div className="details">
+                <h1 className="schoolmap-title">Amar Jyoti Group of Schools and Junior College</h1>
+                <p className="schoolmap-content">Location: Bhayander</p>
+                <p className="schoolmap-content">Address: 184-10(C), God - Dev Navghar Cross Rd, Sai Baba Nagar, Bhayandar East, Mira Bhayandar, Maharashtra. Pin Code: 401105</p>
+                <p className="schoolmap-content">Contact no: 022-28162976 / 28165084</p>
+                </div>
+                <div id="map">
+                <iframe
+                    className="mapiframe"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.327662786657!2d72.85871341438028!3d19.311582949544633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0284ec3aa0d%3A0x6865ee9f090eb9dc!2sAmar%20Jyoti%20Vidya%20Mandir%20High%20School!5e0!3m2!1sen!2sin!4v1625338584415!5m2!1sen!2sin"
+            
+                    allowFullScreen=""
+                    loading="lazy"
+                ></iframe>
+                </div>
+            </div>
+            <div className="Location">
+                <div className="details">
+                <h1 className="schoolmap-title"> B.B.C English High School (Primary & Secondary)</h1>
+                <p className="schoolmap-content">Location: Nalasopara</p>
+                <p className="schoolmap-content">Address:  Commercial Office No. 1,2,3, 1st Flr., Shanti Nagar, 100 Feet,
+            Nalasopara - Virar Link Road, Nr,Shree Prastha, Nalasopara (W), Dist
+            - Palghar</p>
+                <p className="schoolmap-content">Contact no: +91 7719038225</p>
+                </div>
+                <div id="map">
+                <iframe
+                    className="mapiframe"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7525.899268919575!2d72.808799!3d19.414582!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdb54392dcc31ba80!2sBBC%20English%20High%20School!5e0!3m2!1sen!2sin!4v1625338709612!5m2!1sen!2sin"
+          
+                    allowFullScreen=""
+                    loading="lazy"
+                ></iframe>
+                </div>
+            </div>
+            <div className="Location">
+                <div className="details setschooldetail">
+                <h1 className="schoolmap-title">Adarsh Public High School (Primary & Secondary)</h1>
+                <p className="schoolmap-content">Location: Virar</p>
+                <p className="schoolmap-content">Address:  M.B. Kunj building, M.B. Estate, Virar (W).
+                 Dist - Palghar Pin code: 401303</p>
+                <p className="schoolmap-content">Contact no: +91 7741026979</p>
+                </div>
+                <div id="map">
+                <iframe
+                      className="mapiframe"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3762.0363390846546!2d72.8066!3d19.454!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x701318f5542fd7c5!2sAdarsh%20Public%20High%20School!5e0!3m2!1sen!2sus!4v1625338745148!5m2!1sen!2sus"
+                  
+                    allowFullScreen=""
+                    loading="lazy"
+                ></iframe>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
